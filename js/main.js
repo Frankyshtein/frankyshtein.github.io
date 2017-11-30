0c18d608471c0896ed41a1bf007223f48cc916b6
frankyshtein <amid0maru53@gmail.com>
2017-11-30 20:30:52 +0300

    added more repositories to the list; improved aniation of tabs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$("li").on('click',function(){
-    $(this).children("div").slideToggle();
-}); 
+$("li").on('click', function () {
+    if (!$(this).children("div").hasClass("opened")) {
+        
+        $(".opened").slideToggle().removeClass("opened");
+        $(this).children("div").slideToggle().addClass("opened");
+    } else {
+        $(this).children("div").slideToggle().removeClass("opened");
+        console.log("hey")
+    }
+
+});
